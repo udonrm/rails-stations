9fe45d08f1720a9b12e7e8ef91ebebbc0d246bce
udonrm <b201395@hiroshima-u.ac.jp>
2023-10-23 14:47:13 +0000

    映画作品リスト管理ページ（登録）の作成

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,22 @@ class Admin::MoviesController < ApplicationController
     def index
         @movies = Movie.all
     end
+
+    def new
+        @movie = Movie.new
+    end
+
+    def create
+        @movie = Movie.new(movie_params)
+        if @movie.save
+            redirect_to admin_movies_path
+        else
+            render :new
+        end
+    end    
+
+    private
+    def movie_params
+        params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+    end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :year, presence: true
+    validates :description, presence: true
+    validates :image_url, presence: true
+    validates :is_showing, presence: true
 end
