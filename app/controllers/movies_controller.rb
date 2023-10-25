307276a088258e3f56a357730be074e3a4871413
udonrm <b201395@hiroshima-u.ac.jp>
2023-10-25 04:18:21 +0000

    検索機能の追加

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,18 @@
 class MoviesController < ApplicationController
     def index
         @movies = Movie.all
+        
+        # キーワードでの絞り込み
+        if params[:name].present?
+            @movies = @movies.looks(params[:name])
+        end
+        
+        # 公開中または公開予定の絞り込み
+        if params[:is_showing] == 'true'
+            @movies = @movies.where(is_showing: true)
+        elsif params[:is_showing] == 'false'
+            @movies = @movies.where(is_showing: false)
+        end
+        
     end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -4,4 +4,12 @@ class Movie < ApplicationRecord
     validates :description, presence: true
     validates :image_url, presence: true
     validates :is_showing, presence: true
+
+    def self.looks(search)
+        if search
+          self.where("name  = ?", search)
+        else
+          []
+        end
+    end
 end
