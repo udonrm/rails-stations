dd4a459240a9736364dd545ba76397421f09a3e5
udonrm <b201395@hiroshima-u.ac.jp>
2023-10-26 15:05:44 +0900

    座席表の書き方の修正

--- app/controllers/sheets_controller.rb
+++ app/controllers/sheets_controller.rb
@@ -1,7 +1,5 @@
 class SheetsController < ApplicationController
     def index
-        @lineA_sheets = Sheet.where(id: 1..5)
-        @lineB_sheets = Sheet.where(id: 6..10)
-        @lineC_sheets = Sheet.where(id: 11..15)
+        @sheets = Sheet.order(:row, :column).group_by(&:row)
     end
 end
